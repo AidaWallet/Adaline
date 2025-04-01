7eb441063d609f7ad77cec500dbb2879f50a6ed9
AidaWallet <bivreost@gmail.com>
2025-04-01 18:07:15 +0300

    main.py

--- main.py
+++ main.py
@@ -75,4 +75,6 @@ def index():
 
 if __name__ == "__main__":
     Thread(target=post_daily_message).start()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
